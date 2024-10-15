5337332eb97bcccec6f9d432004543534954c91e
Vladimir Petko <vladimir.petko@canonical.com>
2024-10-15 15:15:37 +1300

    chore: delete experimental demos

--- demos/experimental/maven-sample/deploy.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-name=$1
-(cp -r sample/ ${1}
-    cd ${1} &&
-    rockcraft pack &&
-    find . -name *.rock -exec sudo skopeo --insecure-policy copy oci-archive:{} docker-daemon:${1}:latest \;
-    )
--- demos/experimental/spring-boot-framework/app1/deploy.sh
+++
@@ -1,1 +0,0 @@
-sudo skopeo --insecure-policy copy oci-archive:app1_0.1_amd64.rock docker-daemon:app1:latest
